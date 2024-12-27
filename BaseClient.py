38a086d178d79068e021e34e8cdce5405f0a026c
hamletsspeak <gamleturusadze@gmail.com>
2024-12-27 17:03:52 +0300

    2.4 (NEW) Добавил класс для работы с database. Обновил UML диаграмму. Убрал повторение кода из классов Json и Yaml.

--- BaseClient.py
+++ BaseClient.py
@@ -1,13 +1,17 @@
 import re
 import json
 import yaml
+import psycopg2
+from psycopg2 import sql
+from typing import List
 from abc import ABC, abstractmethod
+
 class BaseClientShortInfo:
     def __init__(self, client_id, fullname, document):
         self.set_id(client_id)
         self.set_fullname(fullname)
         self.set_document(document)
-        
+
     @staticmethod
     def __validate_client_id(client_id):
         if not isinstance(client_id, int) or client_id <= 0:
@@ -135,6 +139,7 @@ class BaseClient:
         return BaseClient(
             data['client_id'], data['fullname'], data['document'], data['age'], 
             data['phone_number'], data['address'], data['email'],)
+        
     def to_dict(self):
         return {
             'client_id': self.get_client_id(),
@@ -183,9 +188,10 @@ class BaseClient:
         return str(self.short_info)
 
     def __str__(self):
-            return (f"Client [ID: {self.get_client_id()}, FIO: {self.get_fullname()}, "
-                    f"Document: {self.get_document()}, Age: {self.__age}, Phone_Number: {self.__phone_number}, "
-                    f"Address: {self.__address}, Email: {self.__email}]")
+        return (f"Client [ID: {self.get_client_id()}, FIO: {self.get_fullname()}, "
+                f"Document: {self.get_document()}, Age: {self.__age}, Phone_Number: {self.__phone_number}, "
+                f"Address: {self.__address}, Email: {self.__email}]")
+
     
     def __eq__(self, other):
         if isinstance(other, BaseClient):
@@ -198,52 +204,39 @@ class BaseClient_Rep_Strategy(ABC):
         pass
 
     @abstractmethod
-    def save_all(self):
+    def save_all(self, data):
         pass
 
-    @abstractmethod
-    def get_by_id(self, client_id):
-        pass
-
-    @abstractmethod
-    def get_k_n_short_list(self, k, n):
-        pass
-    
-    @abstractmethod
-    def sort_by_field(self):
-        pass
-    
-    @abstractmethod
-    def add_client(self, fullname, document, age, phone_number, address, email):
-        pass
-
-    @abstractmethod
-    def replace_by_id(self, client_id, new_client):
-        pass
-
-    @abstractmethod
-    def delete_by_id(self, client_id):
-        pass
-    
-class BaseClient_Rep_Json(BaseClient_Rep_Strategy):
-    def __init__(self, file_name):
-        self.file_name = file_name
+    def __init__(self):
         self.clients = self.read_all()
-        
-    def read_all(self):
-        try:
-            with open(self.file_name, 'r') as file:
-                data = json.load(file)
-                return [BaseClient.from_dict(client) for client in data]
-        except FileNotFoundError:
-            return []
 
-    def __save_all(self):
-        data = []
+    def __is_unique(self, document, unverifiable_client_id=None):
         for client in self.clients:
-            data.append(client.to_dict())
-        with open(self.file_name, 'w') as file:
-            json.dump([client.to_dict() for client in self.clients], file, indent=4)
+            if client.get_client_id() != unverifiable_client_id and client.get_document() == document:
+                return False
+        return True
+
+    def add_client(self, fullname, document, age, phone_number, address, email):
+        new_id = max([client.get_client_id() for client in self.clients], default=0) + 1
+        if not self.__is_unique(document):
+            raise ValueError(f"Client with this document already exists.")
+        new_client = BaseClient(new_id, fullname, document, age, phone_number, address, email)
+        self.clients.append(new_client)
+        self.save_all(self.clients)
+
+    def replace_by_id(self, client_id, new_client):
+        if not self.__is_unique(new_client.get_document(), client_id):
+            raise ValueError(f"Client with this document already exists.")
+        for i, client in enumerate(self.clients):
+            if client.get_client_id() == client_id:
+                self.clients[i] = new_client
+                self.save_all(self.clients)
+                return True
+        return False
+
+    def delete_by_id(self, client_id):
+        self.clients = [client for client in self.clients if client.get_client_id() != client_id]
+        self.save_all(self.clients)
 
     def get_by_id(self, client_id):
         for client in self.clients:
@@ -259,109 +252,45 @@ class BaseClient_Rep_Json(BaseClient_Rep_Strategy):
     def sort_by_field(self):
         self.clients.sort(key=lambda client: client.client_id)
 
-    def __is_unique(self, document, unverifiable_client_id=None):
-        if unverifiable_client_id:
-            for client in self.clients:
-                if client.get_client_id() != unverifiable_client_id and client.get_document() == document:
-                    return False
-        else:
-            for client in self.clients:
-                if client.get_document() == document:
-                    return False
-        return True
+class BaseClient_Rep_Json(BaseClient_Rep_Strategy):
+    def __init__(self, filename):
+        self.filename = filename
+        self.clients = self.read_all()
+        
+    def read_all(self):
+        try:
+            with open(self.filename, 'r') as file:
+                data = json.load(file)
+                return [BaseClient.from_dict(client) for client in data]
+        except FileNotFoundError:
+            return []
 
-    def add_client(self, fullname, document, age, phone_number, address, email):
-        new_id = max([client.get_client_id() for client in self.clients], default=0) + 1
-        if not self.__is_unique(document):
-            raise ValueError(f"client with this document already exists.")
-        new_client = BaseClient(new_id, fullname, document, age, phone_number, address, email)
-        self.clients.append(new_client)
-        self.__save_all()
-
-    def replace_by_id(self, client_id, new_client):
-        if not self.__is_unique(new_client.get_document(), client_id):
-            raise ValueError(f"client with this document already exists.")
-        for i, client in enumerate(self.clients):
-            if client.get_client_id() == client_id:
-                self.clients[i] = new_client
-                self.__save_all()
-                return True
-        return False
-
-    def delete_by_id(self, client_id):
-        self.clients = [client for client in self.clients if client.get_client_id() != client_id]
-        self.__save_all()
-
-    def get_count(self):
-        return len(self.clients)
+    def save_all(self, data):
+        with open(self.filename, 'w') as file:
+            json.dump([client.to_dict() for client in data], file, indent=4)
 
 class BaseClient_Rep_Yaml(BaseClient_Rep_Strategy):
     def __init__(self, filename):
         self.filename = filename
         self.clients = self.read_all()
-
+    
     def read_all(self):
         try:
-            with open(self.filename, 'r') as file:
+            with open(self.filename, 'r', encoding='utf-8') as file:
                 data = yaml.safe_load(file)
-                return [client.from_dict(client) for client in data] if data else []
+                print(f"Загруженные данные из YAML: {data}")  # Отладочный вывод
+                return [BaseClient.from_dict(client) for client in data] if data else []
         except FileNotFoundError:
+            print(f"Файл {self.filename} не найден.")
+            return []
+        except yaml.YAMLError as e:
+            print(f"Ошибка при парсинге YAML: {e}")
             return []
 
-    def __save_all(self):
+    def save_all(self, data):
         with open(self.filename, 'w') as file:
-            yaml.safe_dump([client.to_dict() for client in self.clients], file)
+            yaml.safe_dump([client.to_dict() for client in data], file)
 
-    def __is_unique(self, document, unverifiable_client_id=None):
-        if unverifiable_client_id:
-            for client in self.clients:
-                if client.get_client_id() != unverifiable_client_id and client.get_document() == document:
-                    return False
-        else:
-            for client in self.clients:
-                if client.get_document() == document:
-                    return False
-        return True
-
-    def get_by_id(self, client_id):
-        for client in self.clients:
-            if client.get_client_id() == client_id:
-                return client
-        raise ValueError(f"client with ID {client_id} not found.")
-
-    def get_k_n_short_list(self, k, n):
-        start = (k - 1) * n
-        end = start + n
-        return self.clients[start:end]
-
-    def sort_by_field(self):
-        self.clients.sort(key=lambda client: client.client_id)
-
-    def add_client(self, fullname, document, age, phone_number, address, email):
-        new_id = max([client.get_client_id() for client in self.clients], default=0) + 1
-        if not self.__is_unique(document):
-            raise ValueError(f"client with this document already exists.")
-        new_client = BaseClient(new_id, fullname, document, age, phone_number, address, email)
-        self.clients.append(new_client)
-        self.__save_all()
-
-    def replace_by_id(self, client_id, new_client):
-        if not self.__is_unique(new_client.get_document(), client_id):
-            raise ValueError(f"client with this document already exists.")
-        for i, client in enumerate(self.clients):
-            if client.get_client_id() == client_id:
-                self.clients[i] = new_client
-                self.__save_all()
-                return True
-        return False
-
-    def delete_by_id(self, client_id):
-        self.clients = [client for client in self.clients if client.get_client_id() != client_id]
-        self.__save_all()
-
-    def get_count(self):
-        return len(self.clients)
-    
 class BaseClientManagerStrategy:
     def __init__(self, repository_strategy: BaseClient_Rep_Strategy):
         self.repository = repository_strategy
@@ -382,15 +311,112 @@ class BaseClientManagerStrategy:
         return self.repository.read_all()
     
     def get_k_n_short_list(self, k, n):
-        return self.repository.get_k_n_short_list()
+        return self.repository.get_k_n_short_list(k, n)
     
     def sort_by_field(self):
         return self.repository.sort_by_field()
 
-clientFull = BaseClient(1, "Иван Иванов", "1234 123123", 25, "+7 123 123 12 12", "Москва 12", "ivanov@example.com")
-clientShort = BaseClientShortInfo(1, "Иван Иванов", "1234 123123")
-clientEq = BaseClientShortInfo(1, "Иван Иванов", "1234 123123")
+class BaseClientPostgresRep(BaseClient_Rep_Strategy):
+    def __init__(self, db_config):
+        self.db_config = db_config
+        self.connection = psycopg2.connect(**db_config)
+        self.cursor = self.connection.cursor()
+        self.clients = self.read_all()
 
-print("Полная версия", clientFull)
-print("Краткая версия", clientShort)
-print(clientShort==clientEq)
+    def read_all(self) -> List[BaseClient]:
+        self.cursor.execute("SELECT * FROM clients")
+        rows = self.cursor.fetchall()
+        return [BaseClient(client_id=row[0], fullname=row[1], document=row[2], 
+                           age=row[3], phone_number=row[4], address=row[5], email=row[6]) for row in rows]
+
+    def save_all(self, data: List[BaseClient]):
+        self.cursor.execute("DELETE FROM clients")
+        for client in data:
+            self.cursor.execute(
+                """
+                INSERT INTO clients (client_id, fullname, document, age, phone_number, address, email)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                """,
+                (client.get_client_id(), client.get_fullname(), client.get_document(), 
+                 client.get_age(), client.get_phone_number(), client.get_address(), client.get_email())
+            )
+        self.connection.commit()
+
+    def add_client(self, fullname, document, age, phone_number, address, email):
+        new_id = self.get_new_id()
+        if not self.__is_unique(document):
+            raise ValueError(f"Client with this document already exists.")
+        new_client = BaseClient(new_id, fullname, document, age, phone_number, address, email)
+        self.clients.append(new_client)
+        self.save_all(self.clients)
+
+    def replace_by_id(self, client_id, new_client):
+        if not self.__is_unique(new_client.get_document(), client_id):
+            raise ValueError(f"Client with this document already exists.")
+        self.cursor.execute("""
+            UPDATE clients 
+            SET fullname = %s, document = %s, age = %s, phone_number = %s, address = %s, email = %s
+            WHERE client_id = %s
+            """, 
+            (new_client.get_fullname(), new_client.get_document(), new_client.get_age(), 
+             new_client.get_phone_number(), new_client.get_address(), new_client.get_email(), client_id)
+        )
+        self.connection.commit()
+        return True
+
+    def delete_by_id(self, client_id):
+        self.cursor.execute("DELETE FROM clients WHERE client_id = %s", (client_id,))
+        self.connection.commit()
+
+    def get_by_id(self, client_id):
+        self.cursor.execute("SELECT * FROM clients WHERE client_id = %s", (client_id,))
+        row = self.cursor.fetchone()
+        if row:
+            return BaseClient(client_id=row[0], fullname=row[1], document=row[2], 
+                              age=row[3], phone_number=row[4], address=row[5], email=row[6])
+        else:
+            raise ValueError(f"Client with ID {client_id} not found")
+
+    def __is_unique(self, document, unverifiable_client_id=None):
+        self.cursor.execute("SELECT client_id FROM clients WHERE document = %s", (document,))
+        result = self.cursor.fetchone()
+        return result is None or result[0] == unverifiable_client_id
+
+    def get_new_id(self):
+        self.cursor.execute("SELECT MAX(client_id) FROM clients")
+        result = self.cursor.fetchone()
+        return (result[0] or 0) + 1
+
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
+
+db_config = {
+    'dbname': 'clients',
+    'user': 'postgres',
+    'password': '123',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+postgres_repository = BaseClientPostgresRep(db_config)
+client_manager = BaseClientManagerStrategy(postgres_repository)
+
+#client_manager.add_client("Иван Иванов", "1211 111112", 30, "89728845782", "Москва", "ivan2@mail.com")
+
+clients = client_manager.get_all_clients()
+for client in clients:
+    print(client)
+
+postgres_repository.close()
+
+
+#json_repository = BaseClient_Rep_Yaml('clients.yaml')
+
+#client_manager = BaseClientManagerStrategy(json_repository)
+
+#client_manager.add_client("Иван Иванов", "1211 111111", 30, "89728845781", "Москва", "ivan@mail.com")
+
+#clients = client_manager.get_all_clients()
+#for client in clients:
+#    print(client)
